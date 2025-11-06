30d905ddca931800bd14f1e5b8066b8d569414d5
KaoutarMajdoul <majdoulk@gmail.com>
2025-11-06 01:22:46 +0100

    simple data binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { PicComponent } from './pic/pic.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [],
+  imports: [
+    PicComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/pic/pic.component.ts
+++ src/app/pic/pic.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-pic',
@@ -7,6 +7,18 @@ import { Component } from '@angular/core';
   templateUrl: './pic.component.html',
   styleUrl: './pic.component.scss'
 })
-export class PicComponent {
+export class PicComponent implements OnInit{
+  title!: string;
+  description!: string;
+  createdAt!: Date;
+  likes!: number;
+  imageUrl!: string;
 
+  ngOnInit(): void {
+      this.title = "Bientôt les vacances";
+      this.description = "Devinez ma prochaine destination.. un indice : paradisiaque.";
+      this.createdAt = new Date();
+      this.likes = 0;
+      this.imageUrl = "https://www.voyagetanzanie.fr/cdn/tz-public/nungwi_plage-MAX-w1000h600.jpg";
+    }
 }
